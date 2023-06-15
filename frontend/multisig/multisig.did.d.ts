ad119a684b15fb1691589ad973ea5dd566171756
Remco <remco@r3m.codes>
2023-06-15 19:39:58 +0200

    Update declarations

--- frontend/multisig/multisig.did.d.ts
+++ frontend/multisig/multisig.did.d.ts
@@ -1,6 +1,11 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
+export interface Account {
+  'owner' : Principal,
+  'subaccount' : [] | [Uint8Array | number[]],
+}
+export interface Dip20TransferArgs { 'to' : Principal, 'amount' : bigint }
 export type Result = { 'Ok' : string } |
   { 'Err' : string };
 export interface SharedData {
@@ -15,6 +20,21 @@ export type Status = { 'Deadlock' : null } |
   { 'Rejected' : null } |
   { 'Expired' : null } |
   { 'Pending' : null };
+export interface TransactionRequestData {
+  'args' : TransferRequestType,
+  'data' : SharedData,
+  'canister_id' : Principal,
+}
+export interface TransferArgs {
+  'to' : Account,
+  'fee' : [] | [bigint],
+  'memo' : [] | [Uint8Array | number[]],
+  'from_subaccount' : [] | [Uint8Array | number[]],
+  'created_at_time' : [] | [bigint],
+  'amount' : bigint,
+}
+export type TransferRequestType = { 'ICRC1' : TransferArgs } |
+  { 'DIP20' : Dip20TransferArgs };
 export type VoteType = { 'Approve' : null } |
   { 'Reject' : null };
 export interface Votes {
@@ -29,11 +49,17 @@ export type WhitelistRequestType = { 'Add' : Principal } |
   { 'Remove' : Principal };
 export interface _SERVICE {
   '__get_candid_interface_tmp_hack' : ActorMethod<[], string>,
+  'get_transaction_requests' : ActorMethod<
+    [[] | [Status]],
+    Array<TransactionRequestData>
+  >,
   'get_whitelist' : ActorMethod<[], Array<Principal>>,
   'get_whitelist_requests' : ActorMethod<
     [[] | [Status]],
     Array<WhitelistRequestData>
   >,
+  'transaction_request' : ActorMethod<[Principal, TransferRequestType], Result>,
+  'vote_on_transaction_request' : ActorMethod<[number, VoteType], Result>,
   'vote_on_whitelist_request' : ActorMethod<[number, VoteType], Result>,
   'whitelist_request' : ActorMethod<[WhitelistRequestType], Result>,
 }
--- frontend/multisig/multisig.did.js
+++ frontend/multisig/multisig.did.js
@@ -6,9 +6,25 @@ export const idlFactory = ({ IDL }) => {
     'Expired' : IDL.Null,
     'Pending' : IDL.Null,
   });
-  const WhitelistRequestType = IDL.Variant({
-    'Add' : IDL.Principal,
-    'Remove' : IDL.Principal,
+  const Account = IDL.Record({
+    'owner' : IDL.Principal,
+    'subaccount' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const TransferArgs = IDL.Record({
+    'to' : Account,
+    'fee' : IDL.Opt(IDL.Nat),
+    'memo' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'from_subaccount' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'created_at_time' : IDL.Opt(IDL.Nat64),
+    'amount' : IDL.Nat,
+  });
+  const Dip20TransferArgs = IDL.Record({
+    'to' : IDL.Principal,
+    'amount' : IDL.Nat64,
+  });
+  const TransferRequestType = IDL.Variant({
+    'ICRC1' : TransferArgs,
+    'DIP20' : Dip20TransferArgs,
   });
   const Votes = IDL.Record({
     'rejections' : IDL.Vec(IDL.Principal),
@@ -21,19 +37,43 @@ export const idlFactory = ({ IDL }) => {
     'created_at' : IDL.Nat64,
     'requested_by' : IDL.Principal,
   });
+  const TransactionRequestData = IDL.Record({
+    'args' : TransferRequestType,
+    'data' : SharedData,
+    'canister_id' : IDL.Principal,
+  });
+  const WhitelistRequestType = IDL.Variant({
+    'Add' : IDL.Principal,
+    'Remove' : IDL.Principal,
+  });
   const WhitelistRequestData = IDL.Record({
     'request_type' : WhitelistRequestType,
     'data' : SharedData,
   });
-  const VoteType = IDL.Variant({ 'Approve' : IDL.Null, 'Reject' : IDL.Null });
   const Result = IDL.Variant({ 'Ok' : IDL.Text, 'Err' : IDL.Text });
+  const VoteType = IDL.Variant({ 'Approve' : IDL.Null, 'Reject' : IDL.Null });
   return IDL.Service({
     '__get_candid_interface_tmp_hack' : IDL.Func([], [IDL.Text], ['query']),
+    'get_transaction_requests' : IDL.Func(
+        [IDL.Opt(Status)],
+        [IDL.Vec(TransactionRequestData)],
+        ['query'],
+      ),
     'get_whitelist' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
     'get_whitelist_requests' : IDL.Func(
         [IDL.Opt(Status)],
         [IDL.Vec(WhitelistRequestData)],
         ['query'],
+      ),
+    'transaction_request' : IDL.Func(
+        [IDL.Principal, TransferRequestType],
+        [Result],
+        [],
+      ),
+    'vote_on_transaction_request' : IDL.Func(
+        [IDL.Nat32, VoteType],
+        [Result],
+        [],
       ),
     'vote_on_whitelist_request' : IDL.Func([IDL.Nat32, VoteType], [Result], []),
     'whitelist_request' : IDL.Func([WhitelistRequestType], [Result], []),
