7e507d227c745a1b117549c32a87bc360b17290f
Semen Loktionov <loktionov.kh@gmail.com>
2024-05-29 13:42:52 +0300

    fmt

--- src/canister/logic/mod.rs
+++ src/canister/logic/mod.rs
@@ -1,13 +1,13 @@
 pub mod airdrop_logic;
 pub mod consts;
+pub mod owner_logic;
 pub mod proposal_logic;
 pub mod transfer_logic;
 pub mod whitelist_logic;
-pub mod owner_logic;
 
 pub use airdrop_logic::AirdropLogic;
 pub use consts::*;
+pub use owner_logic::OwnerLogic;
 pub use proposal_logic::ProposalLogic;
 pub use transfer_logic::TransferLogic;
 pub use whitelist_logic::WhitelistLogic;
-pub use owner_logic::OwnerLogic;
